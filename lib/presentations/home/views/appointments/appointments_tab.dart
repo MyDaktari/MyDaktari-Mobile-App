aa83f0f4c720b3c8610eb25884ffe26c50c6aa00
ElieBamunoba <eliebamunoba@gmail.com>
2023-05-25 10:05:57 +0300

    appointment tab with cubit

---
+++ lib/presentations/home/views/appointments/appointments_tab.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'doctor_appointment_card.dart';
+
+class AppointmentTab extends StatelessWidget {
+  const AppointmentTab({super.key, required this.list});
+  final List list;
+  @override
+  Widget build(BuildContext context) {
+    return list.isNotEmpty
+        ? ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (context, index) {
+              return DoctorAppointmentCard(
+                doctorAppointment: list.elementAt(index),
+              );
+            })
+        : Center(
+            child: Text('You\'ve got nothing yet'),
+          );
+  }
+}
--- lib/presentations/home/views/appointments/patient_appointments.dart
+++ lib/presentations/home/views/appointments/patient_appointments.dart
@@ -1,26 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_daktari/constants/constants.dart';
-import 'package:my_daktari/presentations/home/views/appointments/doctor_appointment_card.dart';
 
 import '../../../../logic/bloc/doctor_bloc/doctor_appointments/doctor_appointments_bloc.dart';
+import '../../../../logic/cubit/tab_update/tab_update_cubit.dart';
+import 'appointments_tab.dart';
 
 class PatientAppointment extends StatelessWidget {
   PatientAppointment({super.key});
-  final TabNotifier _tabNotifier = TabNotifier(0);
   final PageController _pageController = PageController();
-  List<Widget> tabs(int tabValue) {
-    List<String> titles = ['Today', 'Upcoming', 'Pending', 'History'];
-    return titles.map((title) {
-      int index = titles.indexOf(title);
-      return tab(title, index, tabValue);
-    }).toList();
-  }
 
-  Widget tab(String title, int index, int tabValue) {
+  Widget tab(String title, int index, int tabValue, BuildContext context) {
     return GestureDetector(
       onTap: () {
-        _tabNotifier.newValue = index;
+        context.read<TabUpdateCubit>().setTabValue(index);
         _pageController.animateToPage(index,
             duration: Duration(milliseconds: 200), curve: Curves.linear);
       },
@@ -29,13 +22,10 @@ class PatientAppointment extends StatelessWidget {
           Text(
             title,
             style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: tabValue == index ? primaryColor : Colors.grey,
-            ),
+                fontWeight: FontWeight.bold,
+                color: tabValue == index ? primaryColor : Colors.grey),
           ),
-          SizedBox(
-            height: 5,
-          ),
+          SizedBox(height: 5),
           Container(
             height: 3,
             decoration: BoxDecoration(
@@ -51,114 +41,88 @@ class PatientAppointment extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> tabs(int tabValue) {
+      List<String> titles = ['Today', 'Upcoming', 'Pending', 'History'];
+      return titles.map((title) {
+        int index = titles.indexOf(title);
+        return tab(title, index, tabValue, context);
+      }).toList();
+    }
+
     TextTheme textTheme = Theme.of(context).textTheme;
-    return ValueListenableBuilder(
-        valueListenable: _tabNotifier,
-        builder: (context, tabValue, _) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Appointments',
-                  style: textTheme.titleLarge,
+    return BlocBuilder<TabUpdateCubit, TabUpdateState>(
+      builder: (context, state) {
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Appointments', style: textTheme.titleLarge),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: Text(
+                  'You have the following appointments',
+                  style: textTheme.bodyMedium?.copyWith(fontSize: 21),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: Text(
-                    'You have the following appointments',
-                    style: textTheme.bodyMedium?.copyWith(fontSize: 21),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 8.0),
+                child: SizedBox(
+                  height: 50,
+                  width: double.infinity,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: tabs(state.tabValue)
+                        .map((tab) => Expanded(
+                              child: tab,
+                            ))
+                        .toList(),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 8.0),
-                  child: SizedBox(
-                    height: 50,
-                    width: double.infinity,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: tabs(tabValue)
-                          .map((tab) => Expanded(
-                                child: tab,
-                              ))
-                          .toList(),
-                    ),
-                  ),
-                ),
-                BlocBuilder<DoctorAppointmentsBloc, DoctorAppointmentsState>(
-                  builder: (context, state) {
-                    if (state is DoctorAppointmentsLoading) {
-                      return Center(child: CircularProgressIndicator());
-                    } else if (state is DoctorAppointmentsLoaded) {
-                      return Expanded(
-                          child: PageView(
-                        controller: _pageController,
-                        onPageChanged: (val) {
-                          _tabNotifier.newValue = val;
-                        },
-                        children: [
-                          AppointmentList(list: []),
-                          AppointmentList(list: []),
-                          AppointmentList(
-                              list: state.appointments
-                                  .where((element) =>
-                                      (element.appointmentStatus ?? '')
-                                          .toLowerCase() ==
-                                      'pending')
-                                  .toList()),
-                          AppointmentList(
-                              list: state.appointments
-                                  .where((element) =>
-                                      (element.appointmentStatus ?? '')
-                                          .toLowerCase() ==
-                                      'done')
-                                  .toList()),
-                        ],
-                      ));
-                    } else if (state is DoctorAppointmentsLoadingError) {
-                      return Center(child: Text(state.message));
-                    } else {
-                      return Center(
-                          child: Text(
-                              'An error Occured which fetching your appointment'));
-                    }
-                  },
-                )
-              ],
-            ),
-          );
-        });
-  }
-}
-
-class AppointmentList extends StatelessWidget {
-  const AppointmentList({
-    super.key,
-    required this.list,
-  });
-  final List list;
-  @override
-  Widget build(BuildContext context) {
-    return list.isNotEmpty
-        ? ListView.builder(
-            itemCount: list.length,
-            itemBuilder: (context, index) {
-              return DoctorAppointmentCard(
-                doctorAppointment: list.elementAt(index),
-              );
-            })
-        : Center(
-            child: Text('You\'ve got nothing yet'),
-          );
-  }
-}
-
-class TabNotifier extends ValueNotifier<int> {
-  TabNotifier(int value) : super(value);
-
-  set newValue(int value) {
-    this.value = value;
-    notifyListeners();
+              ),
+              BlocBuilder<DoctorAppointmentsBloc, DoctorAppointmentsState>(
+                builder: (context, state) {
+                  if (state is DoctorAppointmentsLoading) {
+                    return Center(child: CircularProgressIndicator());
+                  } else if (state is DoctorAppointmentsLoaded) {
+                    return Expanded(
+                        child: PageView(
+                      controller: _pageController,
+                      onPageChanged: (val) {
+                        context.read<TabUpdateCubit>().setTabValue(val);
+                      },
+                      children: [
+                        AppointmentTab(list: []),
+                        AppointmentTab(list: []),
+                        AppointmentTab(
+                            list: state.appointments
+                                .where((element) =>
+                                    (element.appointmentStatus ?? '')
+                                        .toLowerCase() ==
+                                    'pending')
+                                .toList()),
+                        AppointmentTab(
+                            list: state.appointments
+                                .where((element) =>
+                                    (element.appointmentStatus ?? '')
+                                        .toLowerCase() ==
+                                    'done')
+                                .toList()),
+                      ],
+                    ));
+                  } else if (state is DoctorAppointmentsLoadingError) {
+                    return Center(child: Text(state.message));
+                  } else {
+                    return Center(
+                        child: Text(
+                            'An error Occured which fetching your appointment'));
+                  }
+                },
+              )
+            ],
+          ),
+        );
+      },
+    );
   }
 }

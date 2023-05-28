ca6174d2f8c0681b027ef852ca70387a7ccdfdec
ElieBamunoba <eliebamunoba@gmail.com>
2023-05-28 21:29:45 +0300

    update

--- lib/repositories/client/client_repository.dart
+++ lib/repositories/client/client_repository.dart
@@ -7,7 +7,8 @@ import 'package:http/http.dart' as http;
 class ClientRepository extends BaseClientRepository {
   @override
   Future<List<DoctorProfileModel>> fetchDoctors() async {
-    final response = await http.get(Uri.parse('your_api_endpoint_here'));
+    final response = await http
+        .get(Uri.parse('https://mydoc.my-daktari.com/new_api/viewDoctors.php'));
 
     if (response.statusCode == 200) {
       final jsonData = json.decode(response.body);
@@ -23,7 +24,8 @@ class ClientRepository extends BaseClientRepository {
 
   @override
   Future<List<DoctorProfileModel>> searchDoctors(String searchTerm) async {
-    final response = await http.post(Uri.parse('your_search_api_endpoint_here'),
+    final response = await http.post(
+        Uri.parse('https://mydoc.my-daktari.com/new_api/searchDoctor.php'),
         headers: {'Content-Type': 'application/json'},
         body: json.encode({'searchTerm': searchTerm}));
 

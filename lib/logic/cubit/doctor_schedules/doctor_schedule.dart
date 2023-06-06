4c4a3b487304871e5c91672b77da53070dfe2fa3
john-muinde <jhnmuinde@gmail.com>
2023-06-06 21:54:27 +0300

    rectified the constraints in the endtime and the startTimes schedule cubit

--- lib/logic/cubit/doctor_schedules/doctor_schedule.dart
+++ lib/logic/cubit/doctor_schedules/doctor_schedule.dart
@@ -1,3 +1,5 @@
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -19,6 +21,21 @@ class ScheduleCubit extends Cubit<List<DaySchedule>> {
   void updateSchedule(DaySchedule schedule) {
     final index = state.indexWhere((s) => s.id == schedule.id);
     if (index != -1) {
+      final timeFormat = DateFormat('h:mm a');
+      final startTime = timeFormat.parse(schedule.startTime);
+      final endTime = timeFormat.parse(schedule.endTime);
+
+      // Check if the start time is earlier than the end time
+      if (startTime.isAfter(endTime)) {
+        Fluttertoast.showToast(msg: 'End time needs to be after start time');
+        return;
+      } // Check if the start time is equal to the end time
+      if (startTime == endTime) {
+        Fluttertoast.showToast(
+            msg: 'Start time cannot be the same as end time');
+        return;
+      }
+
       final updatedSchedule = state[index].copyWith(
         startTime: schedule.startTime,
         endTime: schedule.endTime,
